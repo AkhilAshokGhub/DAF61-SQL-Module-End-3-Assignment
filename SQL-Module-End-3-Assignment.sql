7d82ceca86bb381e8961d166df9b10ac84524f2d
AkhilAshokGhub <145967039+AkhilAshokGhub@users.noreply.github.com>
2026-09-22 13:28:44 +0530

    Update SQL-Module-End-3-Assignment.sql

--- SQL-Module-End-3-Assignment.sql
+++ SQL-Module-End-3-Assignment.sql
@@ -5725,7 +5725,7 @@ SELECT
 FROM customer_churn;
 
 SELECT CityTier, COUNT(*) AS ChurnedCount FROM customer_churn WHERE ChurnStatus = 'Churned' AND PreferredOrderCat = 'Laptop & Accessory' GROUP BY CityTier ORDER BY ChurnedCount DESC LIMIT 1;
-SELECT PreferredPaymentMode, COUNT(*) AS CustomerCount    FROM customer_churn WHERE ChurnStatus = 'Active' GROUP BY PreferredPaymentMode ORDER BY CustomerCount DESC LIMIT 1;
+SELECT PreferredPaymentMode, COUNT(*) AS CustomerCount FROM customer_churn WHERE ChurnStatus = 'Active' GROUP BY PreferredPaymentMode ORDER BY CustomerCount DESC LIMIT 1;
 SELECT SUM(OrderAmountHikeFromlastYear) AS TotalOrderAmountHike FROM customer_churn WHERE MaritalStatus = 'Single'    AND PreferredOrderCat = 'Mobile Phone';
 SELECT ROUND(AVG(NumberOfDeviceRegistered), 2) AS AverageDevices FROM customer_churn WHERE PreferredPaymentMode = 'UPI';
 SELECT
@@ -5745,9 +5745,138 @@ ORDER BY TotalCoupons DESC
 LIMIT 1;
 
 
-SELECT
+SELECT 
     PreferredOrderCat,
     COUNT(*) AS CustomerCount,
     MAX(HoursSpentOnApp) AS MaxHoursSpent
 FROM customer_churn
 GROUP BY PreferredOrderCat;
+
+SELECT MAX(SatisfactionScore)
+FROM customer_churn;
+
+SELECT
+    SUM(OrderCount) AS TotalOrderCount
+FROM customer_churn
+WHERE PreferredPaymentMode = 'Credit Card'
+  AND SatisfactionScore = (
+      SELECT MAX(SatisfactionScore)
+      FROM customer_churn
+  );
+  
+  SELECT
+    ROUND(AVG(SatisfactionScore), 2) AS AverageSatisfactionScore
+FROM customer_churn
+WHERE ComplaintReceived = 'Yes';
+
+SELECT DISTINCT PreferredOrderCat
+FROM customer_churn
+WHERE CouponUsed > 5;
+
+SELECT
+    PreferredOrderCat,
+    COUNT(*) AS CustomerCount
+FROM customer_churn
+WHERE CouponUsed > 5
+GROUP BY PreferredOrderCat;
+
+SELECT
+    PreferredOrderCat,
+    ROUND(AVG(CashbackAmount), 2) AS AverageCashback
+FROM customer_churn
+GROUP BY PreferredOrderCat
+ORDER BY AverageCashback DESC
+LIMIT 3;
+
+SELECT DISTINCT PreferredPaymentMode
+FROM customer_churn
+WHERE Tenure = 10
+  AND OrderCount > 500;
+  
+  SELECT
+    CASE
+        WHEN WarehouseToHome <= 5 THEN 'Very Close Distance'
+        WHEN WarehouseToHome <= 10 THEN 'Close Distance'
+        WHEN WarehouseToHome <= 15 THEN 'Moderate Distance'
+        ELSE 'Far Distance'
+    END AS DistanceCategory,
+    ChurnStatus,
+    COUNT(*) AS CustomerCount
+FROM customer_churn
+GROUP BY
+    CASE
+        WHEN WarehouseToHome <= 5 THEN 'Very Close Distance'
+        WHEN WarehouseToHome <= 10 THEN 'Close Distance'
+        WHEN WarehouseToHome <= 15 THEN 'Moderate Distance'
+        ELSE 'Far Distance'
+    END,
+    ChurnStatus
+ORDER BY DistanceCategory, ChurnStatus;
+
+SELECT AVG(OrderCount)
+FROM customer_churn;
+
+SELECT
+    CustomerID,
+    PreferredOrderCat,
+    OrderCount,
+    CashbackAmount,
+    PreferredPaymentMode
+FROM customer_churn
+WHERE MaritalStatus = 'Married'
+  AND CityTier = 1
+  AND OrderCount > (
+      SELECT AVG(OrderCount)
+      FROM customer_churn
+  );
+  
+  
+  CREATE TABLE customer_returns (
+    ReturnID INT PRIMARY KEY,
+    CustomerID INT,
+    ReturnDate DATE,
+    RefundAmount INT
+);
+
+INSERT INTO customer_returns
+(ReturnID, CustomerID, ReturnDate, RefundAmount)
+VALUES
+(1001, 50022, '2023-01-01', 2130),
+(1002, 50316, '2023-01-23', 2000),
+(1003, 51099, '2023-02-14', 2290),
+(1004, 52321, '2023-03-08', 2510),
+(1005, 52928, '2023-03-20', 3000),
+(1006, 53749, '2023-04-17', 1740),
+(1007, 54206, '2023-04-21', 3250),
+(1008, 54838, '2023-04-30', 1990);
+
+
+SELECT
+    r.ReturnID,
+    r.CustomerID,
+    r.ReturnDate,
+    r.RefundAmount,
+    c.Tenure,
+    c.PreferredLoginDevice,
+    c.CityTier,
+    c.WarehouseToHome,
+    c.PreferredPaymentMode,
+    c.Gender,
+    c.HoursSpentOnApp,
+    c.NumberOfDeviceRegistered,
+    c.PreferredOrderCat,
+    c.SatisfactionScore,
+    c.MaritalStatus,
+    c.NumberOfAddress,
+    c.OrderAmountHikeFromlastYear,
+    c.CouponUsed,
+    c.OrderCount,
+    c.DaySinceLastOrder,
+    c.CashbackAmount,
+    c.ComplaintReceived,
+    c.ChurnStatus
+FROM customer_returns r
+INNER JOIN customer_churn c
+    ON r.CustomerID = c.CustomerID
+WHERE c.ChurnStatus = 'Churned'
+  AND c.ComplaintReceived = 'Yes';
